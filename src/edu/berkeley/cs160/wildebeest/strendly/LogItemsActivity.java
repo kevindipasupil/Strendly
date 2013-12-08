dc911b2b188acb21da0ed7373604b39d2c8e5e77
Kevin Di Pasupil <kdipasupil@berkeley.edu>
2013-12-08 14:13:42 -0800

    Add if condition for setIndicatorBounds in log items activity (relative version only works for a certain sdk).

--- src/edu/berkeley/cs160/wildebeest/strendly/LogItemsActivity.java
+++ src/edu/berkeley/cs160/wildebeest/strendly/LogItemsActivity.java
@@ -19,87 +19,91 @@ import android.widget.ExpandableListView;
 
 public class LogItemsActivity extends Activity {
 
-    ExpandableListAdapter listAdapter;
-    ExpandableListView expListView;
-    List<String> listDataHeader;
-    HashMap<String, List<String>> listDataChild;
- 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_log_items);
- 
-        // get the listview
-        expListView = (ExpandableListView) findViewById(R.id.lvExp);
- 
-        // preparing list data
-        prepareListData();
- 
-        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
- 
-        // setting list adapter
-        expListView.setAdapter(listAdapter);
-        
-        // for moving the group indicator to the right
-        DisplayMetrics metrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(metrics);
-        int width = metrics.widthPixels;
-    	Log.d("WIDTH", "WIDTH IS " + width);
-        //this code for adjusting the group indicator into right side of the view
-        expListView.setIndicatorBoundsRelative(width - GetDipsFromPixel(114), width - GetDipsFromPixel(40));
-    }
-    
-    public void receipt(View v) {
+	ExpandableListAdapter listAdapter;
+	ExpandableListView expListView;
+	List<String> listDataHeader;
+	HashMap<String, List<String>> listDataChild;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_log_items);
+
+		// get the listview
+		expListView = (ExpandableListView) findViewById(R.id.lvExp);
+
+		// preparing list data
+		prepareListData();
+
+		listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+
+		// setting list adapter
+		expListView.setAdapter(listAdapter);
+
+		// for moving the group indicator to the right
+		DisplayMetrics metrics = new DisplayMetrics();
+		getWindowManager().getDefaultDisplay().getMetrics(metrics);
+		int width = metrics.widthPixels;
+		Log.d("WIDTH", "WIDTH IS " + width);
+		//this code for adjusting the group indicator into right side of the view
+		if(android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.JELLY_BEAN_MR2) {
+			expListView.setIndicatorBounds(width - GetDipsFromPixel(114), width - GetDipsFromPixel(40));
+		} else {
+			expListView.setIndicatorBoundsRelative(width - GetDipsFromPixel(114), width - GetDipsFromPixel(40));
+		}
+	}
+
+	public void receipt(View v) {
 		Intent i = new Intent(this, Receipt.class);
 		startActivity(i);
 	}
- 
-    /*
-     * Preparing the list data
-     */
-    private void prepareListData() {
-        listDataHeader = new ArrayList<String>();
-        listDataChild = new HashMap<String, List<String>>();
- 
-        // Adding child data
-        listDataHeader.add("Pizza");
-        listDataHeader.add("Sandwiches");
-        listDataHeader.add("Salads");
- 
-        // Adding child data
-        List<String> pizzas = new ArrayList<String>();
-        pizzas.add("Pepperoni");
-        pizzas.add("Hawaiian");
-        pizzas.add("Meat Lovers");
-        pizzas.add("Combo");
-        pizzas.add("Vegetarian");
-        pizzas.add("Pineapple & Jalapeno");
- 
-        List<String> sandys = new ArrayList<String>();
-        sandys.add("Turkey Cranberry");
-        sandys.add("Chicken Pesto");
-        sandys.add("Veggie");
-        sandys.add("Caprese");
-        sandys.add("Blackened Halibut");
-        sandys.add("Eggplant");
- 
-        List<String> salad = new ArrayList<String>();
-        salad.add("Wedge");
-        salad.add("Cobb");
-        salad.add("Chinese Chicken");
-        salad.add("Mixed Greens");
-        salad.add("Pasta");
- 
-        listDataChild.put(listDataHeader.get(0), pizzas); // Header, Child data
-        listDataChild.put(listDataHeader.get(1), sandys);
-        listDataChild.put(listDataHeader.get(2), salad);
-    }
 
-    public int GetDipsFromPixel(float pixels)
-    {
-        // Get the screen's density scale
-        final float scale = getResources().getDisplayMetrics().density;
-        // Convert the dps to pixels, based on density scale
-        return (int) (pixels * scale + 0.5f);
-    }
+	/*
+	 * Preparing the list data
+	 */
+	private void prepareListData() {
+		listDataHeader = new ArrayList<String>();
+		listDataChild = new HashMap<String, List<String>>();
+
+		// Adding child data
+		listDataHeader.add("Pizza");
+		listDataHeader.add("Sandwiches");
+		listDataHeader.add("Salads");
+
+		// Adding child data
+		List<String> pizzas = new ArrayList<String>();
+		pizzas.add("Pepperoni");
+		pizzas.add("Hawaiian");
+		pizzas.add("Meat Lovers");
+		pizzas.add("Combo");
+		pizzas.add("Vegetarian");
+		pizzas.add("Pineapple & Jalapeno");
+
+		List<String> sandys = new ArrayList<String>();
+		sandys.add("Turkey Cranberry");
+		sandys.add("Chicken Pesto");
+		sandys.add("Veggie");
+		sandys.add("Caprese");
+		sandys.add("Blackened Halibut");
+		sandys.add("Eggplant");
+
+		List<String> salad = new ArrayList<String>();
+		salad.add("Wedge");
+		salad.add("Cobb");
+		salad.add("Chinese Chicken");
+		salad.add("Mixed Greens");
+		salad.add("Pasta");
+
+		listDataChild.put(listDataHeader.get(0), pizzas); // Header, Child data
+		listDataChild.put(listDataHeader.get(1), sandys);
+		listDataChild.put(listDataHeader.get(2), salad);
+	}
+
+	public int GetDipsFromPixel(float pixels)
+	{
+		// Get the screen's density scale
+		final float scale = getResources().getDisplayMetrics().density;
+		// Convert the dps to pixels, based on density scale
+		return (int) (pixels * scale + 0.5f);
+	}
 }
