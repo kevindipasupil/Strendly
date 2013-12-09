13a25ee590b6da6addd69638647c973d2a4605f5
Jack Arenas <j@ckarenas.com>
2013-12-09 13:52:13 -0800

    adding stats java files

--- src/edu/berkeley/cs160/wildebeest/strendly/BarActivity.java
+++ src/edu/berkeley/cs160/wildebeest/strendly/BarActivity.java
@@ -1,7 +1,13 @@
 package edu.berkeley.cs160.wildebeest.strendly;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
+import com.androidplot.xy.LineAndPointFormatter;
+import com.androidplot.xy.PointLabelFormatter;
+import com.androidplot.xy.SimpleXYSeries;
+import com.androidplot.xy.XYPlot;
+import com.androidplot.xy.XYSeries;
 import com.echo.holographlibrary.Bar;
 import com.echo.holographlibrary.BarGraph;
 
@@ -11,28 +17,59 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.view.Menu;
 import android.view.View;
+import android.view.WindowManager;
 
 public class BarActivity extends Activity {
 
+	
+	private XYPlot plot;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_bar);
 		
-		ArrayList<Bar> points = new ArrayList<Bar>();
-		Bar d = new Bar();
-		d.setColor(Color.parseColor("#99CC00"));
-		d.setName("pizza");
-		d.setValue(10);
-		Bar d2 = new Bar();
-		d2.setColor(Color.parseColor("#FFBB33"));
-		d2.setName("bacon");
-		d2.setValue(20);
-		points.add(d);
-		points.add(d2);
 		
-		BarGraph g = (BarGraph)findViewById(R.id.graph);
-		g.setBars(points);
+		
+		 // fun little snippet that prevents users from taking screenshots
+        // on ICS+ devices :-)
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE,
+                                 WindowManager.LayoutParams.FLAG_SECURE);
+ 
+        setContentView(R.layout.activity_bar);
+ 
+        // initialize our XYPlot reference:
+        plot = (XYPlot) findViewById(R.id.mySimpleXYPlot);
+ 
+        // Create a couple arrays of y-values to plot:
+        Number[] series1Numbers = {1, 8, 5, 2, 7, 4};
+        Number[] series2Numbers = {4, 6, 3, 8, 2, 10};
+ 
+        // Turn the above arrays into XYSeries':
+        XYSeries series1 = new SimpleXYSeries(
+                Arrays.asList(series1Numbers),          // SimpleXYSeries takes a List so turn our array into a List
+                SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, // Y_VALS_ONLY means use the element index as the x value
+                "Series1");                             // Set the display title of the series
+ 
+        // same as above
+        XYSeries series2 = new SimpleXYSeries(Arrays.asList(series2Numbers), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Series2");
+ 
+        // Create a formatter to use for drawing a series using LineAndPointRenderer
+        // and configure it from xml:
+        LineAndPointFormatter series1Format = new LineAndPointFormatter(Color.RED, Color.GREEN, Color.BLUE, null);
+
+ 
+        // add a new series' to the xyplot:
+        plot.addSeries(series1, series1Format);
+ 
+        // same as above:
+        LineAndPointFormatter series2Format = new LineAndPointFormatter(Color.RED, Color.GREEN, Color.YELLOW, null);
+
+        plot.addSeries(series2, series2Format);
+ 
+        // reduce the number of range labels
+        plot.setTicksPerRangeLabel(3);
+        plot.getGraphWidget().setDomainLabelOrientation(-45);
 	}
 
 	@Override
