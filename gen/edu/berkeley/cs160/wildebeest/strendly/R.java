bbdb2470412286d0cf2c786afa4465b5f669dc90
Hemant Bhonsle <hbhonsle1@berkeley.edu>
2013-12-09 15:58:58 -0800

    per group history logging added

--- gen/edu/berkeley/cs160/wildebeest/strendly/R.java
+++ gen/edu/berkeley/cs160/wildebeest/strendly/R.java
@@ -17,6 +17,7 @@ public final class R {
         public static final int lightGrey=0x7f060003;
         public static final int mainbutton=0x7f060005;
         public static final int saddlebrown=0x7f060006;
+        public static final int sailerBlue=0x7f060009;
         public static final int seagullBlue=0x7f060008;
         public static final int white=0x7f060000;
         public static final int yellow=0x7f060004;
@@ -30,6 +31,13 @@ public final class R {
          */
         public static final int activity_horizontal_margin=0x7f040000;
         public static final int activity_vertical_margin=0x7f040001;
+        public static final int domain_label_font_size=0x7f040004;
+        public static final int domain_tick_label_font_size=0x7f040007;
+        public static final int legend_text_font_size=0x7f040008;
+        public static final int pie_segment_label_font_size=0x7f040002;
+        public static final int range_label_font_size=0x7f040005;
+        public static final int range_tick_label_font_size=0x7f040006;
+        public static final int title_font_size=0x7f040003;
     }
     public static final class drawable {
         public static final int arrow_down=0x7f020000;
@@ -45,56 +53,72 @@ public final class R {
         public static final int logitems_searchbar=0x7f02000a;
     }
     public static final class id {
-        public static final int action_settings=0x7f090016;
-        public static final int addItem=0x7f090005;
-        public static final int checkout=0x7f090008;
-        public static final int editButton=0x7f09000f;
-        public static final int edit_items=0x7f090001;
-        public static final int existingItems=0x7f090002;
-        public static final int icon=0x7f09000b;
-        public static final int iconAndTitle=0x7f09000a;
-        public static final int itemName=0x7f090003;
-        public static final int itemPrice=0x7f090004;
+        public static final int action_settings=0x7f09001a;
+        public static final int addItem=0x7f090009;
+        public static final int bargraph=0x7f090002;
+        public static final int checkout=0x7f09000c;
+        public static final int editButton=0x7f090013;
+        public static final int edit_items=0x7f090005;
+        public static final int existingItems=0x7f090006;
+        public static final int graph=0x7f090001;
+        public static final int icon=0x7f09000f;
+        public static final int iconAndTitle=0x7f09000e;
+        public static final int itemName=0x7f090007;
+        public static final int itemPrice=0x7f090008;
         public static final int lblListHeader=0x7f090000;
-        public static final int lblListItem=0x7f090014;
-        public static final int logButton=0x7f09000d;
-        public static final int logEntry=0x7f090011;
-        public static final int log_items=0x7f090006;
-        public static final int lvExp=0x7f090009;
-        public static final int minus=0x7f090013;
-        public static final int plus=0x7f090015;
-        public static final int receipt=0x7f090010;
-        public static final int scroll=0x7f090012;
-        public static final int searchbar=0x7f090007;
-        public static final int statsButton=0x7f09000e;
-        public static final int strendly=0x7f09000c;
+        public static final int lblListItem=0x7f090018;
+        public static final int linegraph=0x7f090004;
+        public static final int logButton=0x7f090011;
+        public static final int logEntry=0x7f090015;
+        public static final int log_items=0x7f09000a;
+        public static final int lvExp=0x7f09000d;
+        public static final int minus=0x7f090017;
+        public static final int piegraph=0x7f090003;
+        public static final int plus=0x7f090019;
+        public static final int receipt=0x7f090014;
+        public static final int scroll=0x7f090016;
+        public static final int searchbar=0x7f09000b;
+        public static final int statsButton=0x7f090012;
+        public static final int strendly=0x7f090010;
     }
     public static final class layout {
         public static final int accordion=0x7f030000;
-        public static final int activity_edit_items=0x7f030001;
-        public static final int activity_log_items=0x7f030002;
-        public static final int activity_main=0x7f030003;
-        public static final int activity_receipt=0x7f030004;
-        public static final int list_group=0x7f030005;
-        public static final int list_item=0x7f030006;
+        public static final int activity_bar=0x7f030001;
+        public static final int activity_edit_items=0x7f030002;
+        public static final int activity_line=0x7f030003;
+        public static final int activity_log_items=0x7f030004;
+        public static final int activity_main=0x7f030005;
+        public static final int activity_pie=0x7f030006;
+        public static final int activity_receipt=0x7f030007;
+        public static final int line_point_formatter_with_plf1=0x7f030008;
+        public static final int line_point_formatter_with_plf2=0x7f030009;
+        public static final int list_group=0x7f03000a;
+        public static final int list_item=0x7f03000b;
     }
     public static final class menu {
-        public static final int edit_items=0x7f080000;
-        public static final int log_items=0x7f080001;
-        public static final int main=0x7f080002;
-        public static final int receipt=0x7f080003;
+        public static final int bar=0x7f080000;
+        public static final int edit_items=0x7f080001;
+        public static final int line=0x7f080002;
+        public static final int log_items=0x7f080003;
+        public static final int main=0x7f080004;
+        public static final int pie=0x7f080005;
+        public static final int receipt=0x7f080006;
     }
     public static final class string {
         public static final int Checkout=0x7f050007;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int desc=0x7f050005;
+        public static final int hello_world=0x7f05000c;
         public static final int item1=0x7f050008;
         public static final int item2=0x7f050009;
         public static final int item3=0x7f05000a;
         public static final int searchbar=0x7f050006;
+        public static final int title_activity_bar=0x7f05000b;
         public static final int title_activity_edit_items=0x7f050004;
+        public static final int title_activity_line=0x7f05000e;
         public static final int title_activity_log_items=0x7f050002;
+        public static final int title_activity_pie=0x7f05000d;
         public static final int title_activity_receipt=0x7f050003;
     }
     public static final class style {
