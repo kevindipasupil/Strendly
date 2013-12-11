fff14f1163adbec28bd6ef5ff2ad7059d236d6dc
Hemant Bhonsle <hbhonsle1@berkeley.edu>
2013-12-11 11:25:44 -0800

    fixes

--- gen/edu/berkeley/cs160/wildebeest/strendly/R.java
+++ gen/edu/berkeley/cs160/wildebeest/strendly/R.java
@@ -11,17 +11,17 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int buttonRed=0x7f06000a;
-        public static final int darkGrey=0x7f060002;
-        public static final int grey=0x7f060001;
-        public static final int juniorNavy=0x7f060007;
-        public static final int lightGrey=0x7f060003;
-        public static final int mainbutton=0x7f060005;
-        public static final int saddlebrown=0x7f060006;
-        public static final int sailorBlue=0x7f060009;
-        public static final int seagullBlue=0x7f060008;
-        public static final int white=0x7f060000;
-        public static final int yellow=0x7f060004;
+        public static final int buttonRed=0x7f07000a;
+        public static final int darkGrey=0x7f070002;
+        public static final int grey=0x7f070001;
+        public static final int juniorNavy=0x7f070007;
+        public static final int lightGrey=0x7f070003;
+        public static final int mainbutton=0x7f070005;
+        public static final int saddlebrown=0x7f070006;
+        public static final int sailorBlue=0x7f070009;
+        public static final int seagullBlue=0x7f070008;
+        public static final int white=0x7f070000;
+        public static final int yellow=0x7f070004;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -30,15 +30,15 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
-        public static final int domain_label_font_size=0x7f040004;
-        public static final int domain_tick_label_font_size=0x7f040007;
-        public static final int legend_text_font_size=0x7f040008;
-        public static final int pie_segment_label_font_size=0x7f040002;
-        public static final int range_label_font_size=0x7f040005;
-        public static final int range_tick_label_font_size=0x7f040006;
-        public static final int title_font_size=0x7f040003;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
+        public static final int domain_label_font_size=0x7f060004;
+        public static final int domain_tick_label_font_size=0x7f060007;
+        public static final int legend_text_font_size=0x7f060008;
+        public static final int pie_segment_label_font_size=0x7f060002;
+        public static final int range_label_font_size=0x7f060005;
+        public static final int range_tick_label_font_size=0x7f060006;
+        public static final int title_font_size=0x7f060003;
     }
     public static final class drawable {
         public static final int arrow_down=0x7f020000;
@@ -55,6 +55,7 @@ public final class R {
         public static final int ic_launcher=0x7f02000b;
         public static final int juniornavy_gradient=0x7f02000c;
         public static final int logitems_searchbar=0x7f02000d;
+        public static final int popup_black=0x7f02000e;
     }
     public static final class id {
         public static final int action_settings=0x7f090029;
@@ -139,31 +140,51 @@ public final class R {
         public static final int statistics=0x7f08000a;
     }
     public static final class string {
-        public static final int Checkout=0x7f050011;
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int desc=0x7f05000c;
-        public static final int edit=0x7f05000d;
-        public static final int hello_world=0x7f050015;
-        public static final int item1=0x7f050012;
-        public static final int item2=0x7f050013;
-        public static final int item3=0x7f050014;
-        public static final int log=0x7f05000e;
-        public static final int searchbar=0x7f050010;
-        public static final int stats=0x7f05000f;
-        public static final int title_activity_bar=0x7f050009;
-        public static final int title_activity_edit_items=0x7f050008;
-        public static final int title_activity_line=0x7f05000b;
-        public static final int title_activity_log_items=0x7f050005;
-        public static final int title_activity_login=0x7f050002;
-        public static final int title_activity_main=0x7f050004;
-        public static final int title_activity_pie=0x7f05000a;
-        public static final int title_activity_receipt=0x7f050006;
-        public static final int title_activity_register=0x7f050003;
-        public static final int title_activity_statistics=0x7f050007;
+        public static final int Checkout=0x7f040011;
+        public static final int action_settings=0x7f040001;
+        public static final int app_name=0x7f040000;
+        public static final int desc=0x7f04000c;
+        public static final int edit=0x7f04000d;
+        public static final int hello_world=0x7f040015;
+        public static final int item1=0x7f040012;
+        public static final int item2=0x7f040013;
+        public static final int item3=0x7f040014;
+        public static final int log=0x7f04000e;
+        public static final int searchbar=0x7f040010;
+        public static final int stats=0x7f04000f;
+        public static final int title_activity_bar=0x7f040009;
+        public static final int title_activity_edit_items=0x7f040008;
+        public static final int title_activity_line=0x7f04000b;
+        public static final int title_activity_log_items=0x7f040005;
+        public static final int title_activity_login=0x7f040002;
+        public static final int title_activity_main=0x7f040004;
+        public static final int title_activity_pie=0x7f04000a;
+        public static final int title_activity_receipt=0x7f040006;
+        public static final int title_activity_register=0x7f040003;
+        public static final int title_activity_statistics=0x7f040007;
     }
     public static final class style {
         /** 
+        Base application theme, dependent on API level. This theme is replaced
+        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+    
+
+            Theme customizations available in newer API levels can go in
+            res/values-vXX/styles.xml, while customizations related to
+            backward-compatibility can go here.
+        
+
+        Base application theme for API 11+. This theme completely replaces
+        AppBaseTheme from res/values/styles.xml on API 11+ devices.
+    
+ API 11 theme customizations can go here. 
+
+        Base application theme for API 14+. This theme completely replaces
+        AppBaseTheme from BOTH res/values/styles.xml and
+        res/values-v11/styles.xml on API 14+ devices.
+    
+ API 14 theme customizations can go here. 
+
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
 
@@ -186,13 +207,15 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f050000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
+ Application theme. 
+ All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
-        public static final int activityTitle=0x7f070003;
-        public static final int btnStyleSeagull=0x7f070002;
-        public static final int receiptList=0x7f070004;
+        public static final int AppTheme=0x7f050001;
+        public static final int activityTitle=0x7f050003;
+        public static final int btnStyleSeagull=0x7f050002;
+        public static final int receiptList=0x7f050004;
     }
 }
